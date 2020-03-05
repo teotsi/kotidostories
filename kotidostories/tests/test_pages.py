354e1751b83b907df855aefd08f76831e828dec1
teotsi <teotsi@gmail.com>
2020-03-06 00:42:28 +0200

    Setup tests (#2)
    * began work on tests[ci skip]
    
    * test setup finished

---
+++ kotidostories/tests/test_pages.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+
+import pytest
+
+from kotidostories import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
+
+def test_up(client):
+    """Start with a blank database."""
+    rv = client.get('/')
+    assert 'OK' in rv.status
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kotidostories",
+    version=os.environ.get('TRAVIS_TAG', 'dev'),
+    author="TXC",
+    author_email="teotsi@gmail.com",
+    description="stories for kotidis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/teotsi/kotidostories",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=['Flask==1.1.1',
+                      'Flask-SQLAlchemy==2.4.1',
+                      ],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.6',
+)
