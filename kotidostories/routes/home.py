25be3ada397696d72b90b9f575881b5284584ba9
teotsi <teotsi@gmail.com>
2020-03-06 04:11:48 +0200

    add gitignore add basic template inheritance

--- kotidostories/routes/home.py
+++ kotidostories/routes/home.py
@@ -5,4 +5,4 @@ landing_bp = Blueprint('landing_bp', __name__)
 
 @landing_bp.route('/')
 def landing_page():
-    return render_template("layout.html")
+    return render_template("landing_page.html")
