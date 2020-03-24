ef55a197e75fcfddd8e61fd86972c208f7fd5277
teotsi <teotsi@gmail.com>
2020-03-25 00:13:59 +0200

    follow/unfollow functionality is complete, improved `auth_required` decorator

--- kotidostories/routes/posting.py
+++ kotidostories/routes/posting.py
@@ -14,54 +14,41 @@ posting_bp = Blueprint('posting_bp', __name__, url_prefix='/user/<string:user>/'
 
 @posting_bp.route('posts/', methods=['GET'])
 def get_posts(user=None):
-    user = User.query.filter_by(username=user).first()
-    filter=request.args.get('filter')
-    if user:
-        if filter == 'following':
-            posts = [serialize(post) for followee in user.following for post in followee.posts]
-        else:
-            posts = [serialize(post) for post in user.posts]
-        return jsonify({'posts': posts})
+    user = User.query.filter_by(username=user).first_or_404()
+    filter = request.args.get('filter')
+    if filter == 'following':
+        posts = [serialize(post) for followee in user.following for post in followee.posts if post.published]
     else:
-        return jsonify({'message': 'No such user'}), 403
+        posts = [serialize(post) for post in user.posts if post.published]
+    return jsonify({'posts': posts})
 
 
 @posting_bp.route('posts/', methods=['POST'])
-@auth_required
+@auth_required(authorization=True)
 def upload_post(user=None):
     print("yooo upload")
-    if current_user.username == user:
-        data = request.get_json()
-        content = data.get('content')
-        title = data.get('title')
-        preview = data.get('preview')
-        post = Post(id=str(uuid.uuid4()), user_id=current_user.id, content=content, title=title, preview=preview)
-        db.session.add(post)
-        db.session.commit()
-        return jsonify({"post": serialize(post), "message": "Put post successfully!"})
-    return jsonify({'message': 'No access'}), 403
+    data = request.get_json()
+    content = data.get('content')
+    title = data.get('title')
+    preview = data.get('preview')
+    post = Post(id=str(uuid.uuid4()), user_id=current_user.id, content=content, title=title, preview=preview)
+    db.session.add(post)
+    db.session.commit()
+    return jsonify({"post": serialize(post), "message": "Put post successfully!"})
 
 
 @posting_bp.route('posts/<string:post_id>', methods=['DELETE'])
-@auth_required
+@auth_required(authorization=True)  # users shouldn't be able to delete posts that they haven't authored
 def delete_post(user=None, post_id=None):
-    message = 'You are not the author of this post'
-    code = 403
-    # users shouldn't be able to delete posts that they haven't authored
-    if current_user.username == user:
-        post = Post.query.filter_by(id=post_id).first()
-        if post:
-            db.session.delete(post)
-            db.session.commit()
-            return jsonify({'message': 'Post deleted'})
-        else:
-            message = 'Post doesn\'t exist!'
-            code = 404
-    return jsonify({'message': message}), code
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({'message': 'Post deleted'})
 
 
 @posting_bp.route('posts/<string:post_id>', methods=['PATCH'])
-@auth_required
+@auth_required(authorization=True)
 def update_post(user=None, post_id=None):
     if current_user.username == user:
         post = Post.query.filter_by(id=post_id).first_or_404()
@@ -92,12 +79,26 @@ def get_comments(user=None):
     return jsonify({'comments': comments})
 
 
-@posting_bp.route('/follow')
-@auth_required
+@posting_bp.route('/follow', methods=['GET'])
+@auth_required()
 def follow_user(user=None):
     if current_user.username == user:
-        return jsonify({"message": "Can't follow yourself man"}), 200
+        return jsonify({"message": "Can't follow yourself man"})
     user = User.query.filter_by(username=user).first_or_404()
     user.followers.append(current_user._get_current_object())
     db.session.commit()
     return jsonify({"message": "Follow successful"})
+
+
+@posting_bp.route('/follow', methods=['DELETE'])
+@auth_required()
+def unfollow_user(user=None):
+    if current_user.username == user:
+        return jsonify({"message": "Can't unfollow yourself man"})
+    user = User.query.filter_by(username=user).first_or_404()
+    try:
+        user.followers.remove(current_user._get_current_object())
+        db.session.commit()
+    except ValueError:
+        return jsonify({'message': 'No follow in the first place'})
+    return jsonify({"message": "Unfollow successful"})
